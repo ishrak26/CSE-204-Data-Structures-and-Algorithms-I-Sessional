28717737942128bfd5684d656e5bb8897541ddcc
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2021-12-17 10:30:12 +0600

    Add necessary comments

--- Offline 04/BST.cpp
+++ Offline 04/BST.cpp
@@ -40,27 +40,27 @@ class BST {
         }
     }
 
-    Node *findhelp(const Key key, Node *curr) {
+    Node *findhelp(const Key key, Node *curr) { // finds a node with key=key in the subtree rooted at curr
         if (curr == NULL) return NULL;
         if (key == curr->key) return curr;
         if (key < curr->key) return findhelp(key, curr->left);
         return findhelp(key, curr->right);
     }
 
-    Node *find_parenthelp(Node *curr, Node *child) {
+    Node *find_parenthelp(Node *curr, Node *child) { // finds parent of a node in a subtree rooted at curr
         assert(curr != NULL);
         if (child->key == curr->left->key || child->key == curr->right->key) return curr;
         if (child->key < curr->key) return find_parenthelp(curr->left, child);
         return find_parenthelp(curr->right, child);
     }
 
-    Node *find_min(Node *curr) {
+    Node *find_min(Node *curr) { // finds minimum val in a subtree rooted at curr
         assert(curr != NULL);
         if (curr->left == NULL) return curr;
         return find_min(curr->left);
     }
 
-    void removehelp(const Key key) {
+    void removehelp(const Key key) { // removes a node with key=key
         // find the node
         Node *node = findhelp(key, root);
         assert(node != NULL); // node exists
@@ -107,28 +107,28 @@ class BST {
         }
     }
 
-    void in_orderhelp(Node *root) {
+    void in_orderhelp(Node *root) { // traverse the subtree rooted at root in-order
         if (root == NULL) return;
         in_orderhelp(root->left);
         cout << root->val << " ";
         in_orderhelp(root->right);
     }
 
-    void pre_orderhelp(Node *root) {
+    void pre_orderhelp(Node *root) { // traverse the subtree rooted at root pre-order
         if (root == NULL) return;
         cout << root->val << " ";
         pre_orderhelp(root->left);
         pre_orderhelp(root->right);
     }
 
-    void post_orderhelp(Node *root) {
+    void post_orderhelp(Node *root) { // traverse the subtree rooted at root post-order
         if (root == NULL) return;
         post_orderhelp(root->left);
         post_orderhelp(root->right);
         cout << root->val << " ";
     }
 
-    void print_treehelp(Node *root) {
+    void print_treehelp(Node *root) { // prints the subtree rooted at root
         if (root == NULL) return;
         cout << root->val;
         if (isLeaf(root)) return;
@@ -153,6 +153,7 @@ public:
         else inserthelp(key, val, root);
     }
 
+    // returns false if key is not present in the tree, true otherwise
     bool remove(const Key key) {
         if(find(key)) {
             removehelp(key);
