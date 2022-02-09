da0070f07a65a023ab1115b9f9949c2d11a63367
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2022-02-09 19:07:13 +0600

    Add LCS

---
+++ Offline 09 (DP)/Problem1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<cassert>
+
+using namespace std;
+
+int LCS(vector<vector<int>>& dp, int m, int n, const string &a, const string &b) {
+    for (int i = 0; i <= m; i++)
+        dp[i][0] = 0;
+    for (int j = 1; j <= n; j++)
+        dp[0][j] = 0;
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (a[i] == b[j])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[m][n];
+}
+
+string backtrack(const vector<vector<int>>& dp, int i, int j, const string &a, const string &b) {
+    if (i == 0 || j == 0)
+        return "\0";
+    if (a[i] == b[j])
+        return backtrack(dp, i-1, j-1, a, b) + a[i];
+    if (dp[i-1][j] == dp[i][j])
+        return backtrack(dp, i-1, j, a, b);
+    assert(dp[i][j-1] == dp[i][j]);
+    return backtrack(dp, i, j-1, a, b);
+}
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    int m = a.length();
+    int n = b.length();
+
+    a = '0' + a; // to make the string 1-indexed
+    b = '0' + b;
+
+    vector<vector<int>> dp(m+1, vector<int> (n+1));
+    int lcs_len = LCS(dp, m, n, a, b);
+    string str = backtrack(dp, m, n, a, b);
+
+    cout << lcs_len << '\n' << str << '\n';
+
+    return 0;
+}
